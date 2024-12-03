0ce6b77b8c24677d1592fb550b2ee8aed1114e84
uzzalahmed23 <uzzalahmed0001@gmail.com>
2024-12-03 09:06:50 +0600

    fixed cta section

--- src/components/app/layout/layout.tsx
+++ src/components/app/layout/layout.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import CTA from "../shared/cta";
 
 function Layout({ children }) {
   return (
     <>
       <Header />
       {children}
+      <CTA/>
       <Footer />
     </>
   );
--- src/components/app/shared/cta.tsx
+++ src/components/app/shared/cta.tsx
@@ -1,14 +1,15 @@
+import { FC } from "react";
 import { ArrowRight } from "lucide-react";
 import { Button } from "./frontend-button";
 import footerImg from "@/assets/footer-img.png";
 
-function CTA() {
+const CTA: FC = () => {
   return (
-    <div className="container">
+    <div className="container relative -bottom-20">
       <div className="bg-primary grid lg:grid-cols-2 items-center gap-8 p-6 rounded-3xl">
-        {/* video  */}
+        {/* Video Section */}
         <div className="lg:px-20">
-          <h3 className="text-[40px] font-semibold  text-white mb-8">
+          <h3 className="text-[40px] font-semibold text-white mb-8">
             Revolutionize Your <br /> Sales Process?
           </h3>
           <p className="text-[#F5F7F9] text-lg leading-[140%] mb-10">
@@ -16,10 +17,12 @@ function CTA() {
             sales development. Unlock your team's potential and boost your
             revenue!
           </p>
-          <Button variant={"footer"}>
+          <Button variant="footer">
             Start your free trial <ArrowRight className="h-8 w-8 shrink-0" />
           </Button>
         </div>
+
+        {/* Image Section */}
         <div>
           <div className="aspect-[616/402] lg:ml-auto max-w-[616px]">
             <img className="size-full" src={footerImg} alt="footer top image" />
@@ -28,6 +31,6 @@ function CTA() {
       </div>
     </div>
   );
-}
+};
 
 export default CTA;
